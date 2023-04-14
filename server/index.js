bb5cf63bfa8af270fc47811e96c2e168266b9d99
Vaibhavihole31 <itspinki05@gmail.com>
2023-04-14 13:12:19 +0530

    basic setup �

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const connnectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGODB_URI);
+  if(conn) {
+    console.log('MongoDB connected successfully!🌼💙🥳');
+  }
+}
+connnectDB();
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
